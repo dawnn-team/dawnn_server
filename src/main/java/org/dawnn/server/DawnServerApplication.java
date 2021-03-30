2d51ab93412a5d5323973985c9e66fe4239d947e
mhmyesman <41195669+mhmyesman@users.noreply.github.com>
2021-03-30 12:22:29 -0400

    Added FCM messaging.

--- src/main/java/org/dawnn/server/DawnServerApplication.java
+++ src/main/java/org/dawnn/server/DawnServerApplication.java
@@ -1,7 +1,16 @@
 package org.dawnn.server;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 /**
  * Uses Spring to run the Dawn server
@@ -9,9 +18,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DawnServerApplication {
 
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigPath;
+
     public static void main(String[] args) {
         SpringApplication.run(DawnServerApplication.class, args);
     }
 
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        // TODO Move this method somewhere else?
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream()))
+                .build();
+
+
+        FirebaseApp app = FirebaseApp.initializeApp(options);
+
+        return FirebaseMessaging.getInstance(app);
+    }
 }
 
--- src/main/java/org/dawnn/server/service/firebase/FCMService.java
+++ src/main/java/org/dawnn/server/service/firebase/FCMService.java
@@ -1,9 +1,12 @@
 package org.dawnn.server.service.firebase;
 
+import com.google.firebase.database.annotations.NotNull;
+import com.google.firebase.database.annotations.Nullable;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+import org.dawnn.server.model.Platform;
 import org.dawnn.server.service.MessageContent;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +23,17 @@ public class FCMService {
         this.firebaseMessaging = firebaseMessaging;
     }
 
-    public String sendMessage(MessageContent messageContent, String token) throws FirebaseMessagingException {
-
+    /**
+     * Send a message to a user at specified token.
+     *
+     * @param messageContent The content of the message to send.
+     * @param token          The recipient of the message.
+     * @param platform       The target users' platform.
+     * @return The ID of the message.
+     * @throws FirebaseMessagingException - If an error occurs during sending.
+     */
+    public String sendMessage(@NotNull MessageContent messageContent,
+                              @NotNull String token, @Nullable Platform platform) throws FirebaseMessagingException {
         // TODO Build notification in relation to the user platform?
 
         // Construct client notification first
