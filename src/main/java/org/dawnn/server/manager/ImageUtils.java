50014ce0158dc34250aceb6f7382d31e0838f986
mhmyesman <41195669+mhmyesman@users.noreply.github.com>
2021-03-30 14:57:38 -0400

    Remove deprecated code + clean.

--- src/main/java/org/dawnn/server/manager/ImageUtils.java
+++ src/main/java/org/dawnn/server/manager/ImageUtils.java
@@ -2,7 +2,6 @@ package org.dawnn.server.manager;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
-import com.drew.lang.GeoLocation;
 import com.drew.metadata.Metadata;
 import lombok.NonNull;
 
@@ -49,17 +48,11 @@ public class ImageUtils {
      * @param image The image to convert.
      * @return A base64 string of the image, or null if conversion failed.
      */
-    public static String toBase64(@NonNull File image) {
-        try {
-            BufferedImage bufferedImage = ImageIO.read(image);
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            ImageIO.write(bufferedImage, "jpg", outputStream);
-            return Arrays.toString(outputStream.toByteArray());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+    public static String toBase64(@NonNull File image) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(image);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "jpg", outputStream);
+        return Arrays.toString(outputStream.toByteArray());
     }
 
     /**
@@ -70,39 +63,11 @@ public class ImageUtils {
      * @return An image equivalent of the provided String, null if conversion fails.
      */
     @Deprecated
-    public static Image fromBase64(@NonNull String base64) {
-        try {
-            byte[] bytes = base64.getBytes(StandardCharsets.UTF_8);
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-            return ImageIO.read(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    public static Image fromBase64(@NonNull String base64) throws IOException {
+        byte[] bytes = base64.getBytes(StandardCharsets.UTF_8);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+        return ImageIO.read(inputStream);
 
-    /**
-     * Find the {@link GeoLocation} of an image {@link Image}
-     *
-     * @param image The image to find the location of.
-     * @return The whatever geolocation that was found, null if none.
-     */
-    @Deprecated
-    public static GeoLocation findLocation(Image image) {
-        // TODO Finish this.
-        return null;
-    }
-
-    /**
-     * Find the {@link GeoLocation} of an image {@link Image}
-     *
-     * @param string64 The string64 interpretation of an image
-     * @return The whatever geoLocation that was found, null if none.
-     */
-    @Deprecated
-    public static GeoLocation findLocation(String string64) {
-        // TODO Finish this.
-        return null;
     }
 
 }
