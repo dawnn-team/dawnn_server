c131d666f4709a68c9dd04830c06159c0d40876f
mhmyesman <41195669+mhmyesman@users.noreply.github.com>
2021-03-31 01:57:45 -0400

    FCM initializes with or without JSON env var.
    This adds support to GitHub runners - they shouldn't use JSON env vars.

--- src/main/java/org/dawnn/server/DawnServerApplication.java
+++ src/main/java/org/dawnn/server/DawnServerApplication.java
@@ -4,11 +4,13 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -19,7 +21,7 @@ import java.io.IOException;
 public class DawnServerApplication {
 
     @Value("${app.firebase-configuration-file-var}")
-    private String firebaseConfigPath;
+    private String firebaseAuthPath;
 
     public static void main(String[] args) {
         SpringApplication.run(DawnServerApplication.class, args);
@@ -29,8 +31,34 @@ public class DawnServerApplication {
     FirebaseMessaging firebaseMessaging() throws IOException {
         // TODO Move this method somewhere else?
 
+        // GitHub runners shouldn't use JSON env vars,
+        // so let's check if AUTH_HOME (JSON env var) exists
+        String authData = null;
+        if (System.getenv(firebaseAuthPath) == null) {
+            // It doesn't exist - we're on a GitHub runner.
+            // Let's build auth data manually.
+
+            authData = new JSONObject()
+                    .put("type", System.getenv("type"))
+                    .put("project_id", System.getenv("project_id"))
+                    .put("private_key_id", System.getenv("private_key_id"))
+                    .put("private_key", System.getenv("private_key"))
+                    .put("client_email", System.getenv("client_email"))
+                    .put("client_id", System.getenv("client_id"))
+                    .put("auth_uri", System.getenv("auth_uri"))
+                    .put("token_uri", System.getenv("token_uri"))
+                    .put("auth_provider_x509_cert_url", System.getenv("auth_provider_x509_cert_url"))
+                    .put("client_x509_cert_url", System.getenv("client_x509_cert_url"))
+                    .toString();
+        }
+
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(System.getenv(firebaseConfigPath))))
+                .setCredentials(GoogleCredentials
+                        .fromStream(
+                                authData == null ?
+                                        new FileInputStream(System.getenv(firebaseAuthPath)) :
+                                        new ByteArrayInputStream(authData.getBytes())
+                        ))
                 .build();
 
         FirebaseApp app = FirebaseApp.initializeApp(options);
