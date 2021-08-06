fe10f461f3264f8ad52b0bd05b5cd649342851cf
mhmyesman <41195669+mhmyesman@users.noreply.github.com>
2021-08-06 16:46:41 -0400

    Initialize FirebaseApp after entry.

--- src/main/java/org/dawnn/server/DawnnServerApplication.java
+++ src/main/java/org/dawnn/server/DawnnServerApplication.java
@@ -1,17 +1,13 @@
 package org.dawnn.server;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -20,18 +16,19 @@ import java.io.IOException;
 @SpringBootApplication
 public class DawnnServerApplication {
 
-
     /**
      * This is how far the user should be able to see markers around
      * themselves, in kilometers.
      */
     public static final double USER_VIEW_DISTANCE = 1.0;
+    private static final Logger logger = LoggerFactory.getLogger(DawnnServerApplication.class);
 
-    @Value("${app.firebase-configuration-file-var}")
-    private String firebaseAuthPath;
+    public DawnnServerApplication() {
+        FirebaseApp.initializeApp();
+        logger.info("Initialized FirebaseApp.");
+    }
 
     public static void main(String[] args) {
-        FirebaseApp.initializeApp();
         SpringApplication.run(DawnnServerApplication.class, args);
     }
 
